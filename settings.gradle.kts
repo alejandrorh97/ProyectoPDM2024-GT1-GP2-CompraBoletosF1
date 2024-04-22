6a0cfe0755c9befc5583d15ad47cabd7a704796e
Alejandro Rivas <alejandrorrh97@gmail.com>
2024-04-21 22:37:10 -0600

    Commit inicial

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ProyectoPDM2024-GT1-Grupo2-CompraBoletosFormula1"
+include(":app")
