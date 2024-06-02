6bcfebd97dfb586772b98388b24d068937d121c1
Alejandro Rivas <alejandrorrh97@gmail.com>
2024-06-02 17:13:50 -0600

    cambio de endpoint y token

--- app/src/main/java/com/ues/boletos/api/BaseService.kt
+++ app/src/main/java/com/ues/boletos/api/BaseService.kt
@@ -6,8 +6,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object BaseService {
-    private const val BASE_URL = "http://172.20.0.6/api/"
-    private const val TOKEN = "1|0JImqZ5WmusMd908QgEzkXASYhpfZIaitwYh7YOsdb9000d3"
+    private const val BASE_URL = "http://3.144.23.33/api"
+    private const val TOKEN = "1|u7Nk2KqknqhJfSp4BZTHL0KIRsyJdE8kfd25fLwg6509addb"
 
     private val client = OkHttpClient.Builder()
         .addInterceptor(AuthInterceptor(TOKEN))
